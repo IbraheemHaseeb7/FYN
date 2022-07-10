769f200c80e89280805bdfebed013f3de404945c
Ibraheem Haseeb <ibraheemhaseeb7@gmail.com>
2022-07-10 18:20:42 +0500

    forums

--- pages/forums/index.js
+++ pages/forums/index.js
@@ -1,18 +1,100 @@
 import styles from "../../styles/forums.module.css";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import Footer from "../../components/footer/footer";
+import { useState } from "react";
 
-const forums = [{ date: "", content: "" }];
+let forums = [
+  {
+    date: "22 June, 2022",
+    question: "How long is this course?",
+    answer:
+      "This is a 3 month long course where you will be guided not only how to refrain from pornography but also you will be given a series of diets and exercise that will help you build a perfect character",
+    link: "https://www.youtube.com",
+    open: false,
+  },
+  {
+    date: "22 June, 2022",
+    question: "How long is this course?",
+    answer:
+      "This is a 3 month long course where you will be guided not only how to refrain from pornography but also you will be given a series of diets and exercise that will help you build a perfect character",
+    link: "https://www.youtube.com",
+    open: false,
+  },
+  {
+    date: "22 June, 2022",
+    question: "How long is this course?",
+    answer:
+      "This is a 3 month long course where you will be guided not only how to refrain from pornography but also you will be given a series of diets and exercise that will help you build a perfect character",
+    link: "https://www.youtube.com",
+    open: false,
+  },
+];
 
 export default function Forums() {
+  const [render, setRender] = useState([
+    {
+      date: "22 June, 2022",
+      question: "How long is this course?",
+      answer:
+        "This is a 3 month long course where you will be guided not only how to refrain from pornography but also you will be given a series of diets and exercise that will help you build a perfect character",
+      link: "https://www.youtube.com",
+      open: false,
+    },
+    {
+      date: "22 June, 2022",
+      question: "How long is this course?",
+      answer:
+        "This is a 3 month long course where you will be guided not only how to refrain from pornography but also you will be given a series of diets and exercise that will help you build a perfect character",
+      link: "https://www.youtube.com",
+      open: false,
+    },
+    {
+      date: "22 June, 2022",
+      question: "How long is this course?",
+      answer:
+        "This is a 3 month long course where you will be guided not only how to refrain from pornography but also you will be given a series of diets and exercise that will help you build a perfect character",
+      link: "https://www.youtube.com",
+      open: false,
+    },
+  ]);
+
+  let index = -1;
+
+  console.log(render);
   return (
     <div className={styles.forums_container}>
       <div className={styles.title_container}>
-        <h1>Get in touch with a Wonderful Community</h1>
+        <h1>Community Forum</h1>
+        <p>
+          A place where all of your questions will be answered and you are
+          welcome to share your opinion on them. You only need to sign up that
+          is free and allows you to keeo your identity hidden using our
+          anonymous account handling.
+        </p>
       </div>
       <div className={styles.forums}>
-        {forums.map(({ date, content }) => {
-          return <div className={styles.forum}></div>;
+        {render.map(({ date, question, open, answer }) => {
+          index++;
+
+          return (
+            <div
+              className={styles.forum}
+              onClick={() => {
+                setRender([...render, {}]);
+              }}
+            >
+              <p>{question}</p>
+              <ArrowDropDownIcon />
+              {open && <p>{answer}</p>}
+              {open && <span>{date}</span>}
+            </div>
+          );
         })}
+        <button type="button" className={styles.load_more}>
+          Load More
+        </button>
       </div>
+      <Footer />
     </div>
   );
 }
