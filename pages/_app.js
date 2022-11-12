b404e6e1304431f1a4bcfa28a17fa97097d48e44
IbraheemHaseeb7 <81369271+IbraheemHaseeb7@users.noreply.github.com>
2022-11-12 11:09:14 +0500

    removing block

--- pages/_app.js
+++ pages/_app.js
@@ -24,7 +24,7 @@ function MyApp({ Component, pageProps }) {
         notification,
       }}
     >
-      <Block />
+      {/*<Block />*/}
       <Toaster />
       <Navbar />
       <Component {...pageProps} />
